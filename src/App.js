8ad2324d0d68fbf27cf0e0549f3c725bad6be19c
Mateusz Nawrocki <mateusz.nawroo@gmail.com>
2021-11-16 22:37:58 +0100

    added function to add new tasks

--- src/App.js
+++ src/App.js
@@ -10,11 +10,7 @@ import { useState } from "react";
 function App() {
 
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([
-    { id: 1, content: "przejść na Reacta", done: true },
-    { id: 2, content: "zjeść obiad", done: true },
-    { id: 3, content: "ogarnąc Reacta", done: false },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
@@ -38,13 +34,24 @@ function App() {
     setTasks(tasks => tasks.map(task => ({ ...task, done: true })));
   };
 
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content: content,
+        done: false,
+        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
+      },
+    ])
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
 
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form />}
+        body={<Form addNewTask={addNewTask} />}
       />
 
       <Section
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,27 @@
 import "./style.css";
+import { useState } from "react";
 
-const Form = () => (
-    <form className="form">
-        <input type="text" className="form__input" autoFocus placeholder="Co jest do zrobienia?" />
+const Form = ({ addNewTask }) => {
+
+    const [newTaskContent, setNewTaskContent] = useState("");
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        if (newTaskContent !== "") {
+            addNewTask(newTaskContent.trim());
+            setNewTaskContent("");
+        } else
+            return null;
+
+    };
+
+    return <form className="form" onSubmit={onFormSubmit}>
+        <input
+            value={newTaskContent}
+            onChange={({ target }) => setNewTaskContent(target.value)}
+            type="text" className="form__input" autoFocus placeholder="Co jest do zrobienia?" />
         <button id="submit" className="form__button">Dodaj zadanie</button>
     </form>
-);
+};
 
 export default Form;
